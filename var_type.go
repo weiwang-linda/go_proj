5ddbde00eead9559d2736fcf5390dc1080b44da2
weiwang <weiwang@dhcp-10-187.nay.redhat.com>
2017-09-12 14:50:25 +0800

    add ':=' for declaration and assign

--- var_type.go
+++ var_type.go
@@ -11,10 +11,12 @@ func main() {
 	var a int
 	var b bool
 
-	/* value to variable */
+	/* assign value to variable */
 	a = 15
 	b = false
+	f := 3.14      //declaration and assign value in one line.
 
 	fmt.Println(strconv.Itoa(a))       //Println include "\n"
-	fmt.Println(strconv.FormatBool(b))
+	fmt.Println(strconv.FormatBool(b))   //convert bool to strong
+	fmt.Println(strconv.FormatFloat(f, 'E', -1, 64))    //convert float to string, ParseFloat(f,64) is convert string to float
 }
