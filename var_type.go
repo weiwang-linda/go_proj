3aafb0a3df8d5f239b190d590fdeae4d0d07dd80
weiwang <weiwang@dhcp-10-187.nay.redhat.com>
2017-09-12 16:09:03 +0800

    add some comment

--- var_type.go
+++ var_type.go
@@ -15,6 +15,13 @@ func main() {
 	a = 15
 	b = false
 	f := 3.14      //declaration and assign value in one line.
+	var (
+		x int
+		y bool
+	)                //like import and const
+
+	m, n := 20, 16    //assign two variables in one time.
+	_, t := 34, 35    //_ is special varialbe, any value assigned to it will be lost.
 
 	fmt.Println(strconv.Itoa(a))       //Println include "\n"
 	fmt.Println(strconv.FormatBool(b))   //convert bool to strong
