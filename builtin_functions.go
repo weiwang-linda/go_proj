340525578853ce2045baaaf9c0e90d763ccef571
weiwang <weiwang@dhcp-10-187.nay.redhat.com>
2017-09-13 18:10:48 +0800

    builtin functions examples

---
+++ builtin_functions.go
@@ -0,0 +1,23 @@
+/* builtin function: no need to import any package */
+
+/* 1. close: close channel for intermedia */
+
+/* 2. delete: map for deleting instance */
+
+/* 3. cap: slice, array and function */
+
+/* 4. new: alloc for all kinds of memory */
+
+/* 5. make: builtin type for alloc memory */
+
+/* 6. copy: slice for copy */
+
+/* 7. append: slice for append */
+
+/* 8. panic/recover: exception */
+
+/* 9. print/println: print low level log */
+
+/* 10. complex, real and imag: complex */
+
+//TODO give some example to using these builtin functions
